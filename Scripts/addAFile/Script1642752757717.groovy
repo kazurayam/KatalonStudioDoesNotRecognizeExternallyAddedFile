03ad7269191b521720386a0335777a52559c7a7c
kazurayam <kazuaki.urayama@gmail.com>
2022-01-21 17:19:15 +0900

    first commit

---
+++ Scripts/addAFile/Script1642752757717.groovy
@@ -0,0 +1,12 @@
+File f = new File("./data.txt")
+f.text = "Hello, world"
+
+/*
+ * 1. execute this Test Case "addAFile",
+ * 2. in the Tests Explorer, the file does not appear automatically
+ * 3. click the Test Explorer pane, right click to show a drop down menu, select "Refesh"
+ * 4. you natucally expect the file to appear
+ * 5. but acutally it doesnt
+ * 6. you want to close this project, and reopen it
+ * 7. then you will find the file appearing in the Test Explorer
+ */
